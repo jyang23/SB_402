d6602ac0fd52b2bf90c4bddf9625ae96bbc9ed92
Justin <justinyang1000@hotmail.com>
2019-07-16 10:44:53 -0400

    Initial commit

---
+++ src/main/java/com/jy/springboot_402/Springboot402Application.java
@@ -0,0 +1,13 @@
+package com.jy.springboot_402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot402Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot402Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jy/springboot_402/Springboot402ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jy.springboot_402;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot402ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
